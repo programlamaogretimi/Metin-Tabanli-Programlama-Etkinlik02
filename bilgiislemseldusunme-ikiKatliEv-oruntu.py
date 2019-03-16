d83d152dab22954eab841a30bb7bbc8a50c72d03
Metin Tabanlı Programlama Öğretimi <45932891+programlamaogretimi@users.noreply.github.com>
2019-03-16 20:00:54 +0300

    Add files via upload

---
+++ bilgiislemseldusunme-ikiKatliEv-oruntu.py
@@ -0,0 +1,75 @@
+from etkinlik import *
+
+for i in range(2):
+    for j in range(4):
+    #kare çizimi
+        ciz(50) #50 birim kenar çiz
+        solaDon() #sol yöne dön
+    #bir sonraki katın çizilmeye başlanacağı
+    #noktaya git
+    solaDon()#kuzey yönüne dön
+    git(50)  #Karenin sol üst köşesine git
+    sagaDon()#Üçgen çizimine başlamak için
+                 #doğu yönüne dön
+#üçgen çizimi
+ciz(50)      #50 birim üçgen
+             #tabanını çiz
+ucgenSag(50) #50 birim üçgen
+             #sağ kenarını çiz
+ucgenSol(50) #50 birim üçgen
+             #sol kenarını çiz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#dogu()
+#git(50)
+#kuzey()
+#git(50)
+
+
+#ciz(50)
+#bati()
+#ciz(50)
+#guney()
+#ciz(50)
+
+
+#catiSag(50)
+#git(50)
+#turtle.seth(240)
+#turtle.fd(50)
+#catiSol(50)
+
