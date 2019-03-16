5f54a664fe652d091de6b5acc024d267e0831795
Metin Tabanlı Programlama Öğretimi <45932891+programlamaogretimi@users.noreply.github.com>
2019-03-16 20:02:06 +0300

    Add files via upload

---
+++ bilgiislemseldusunme-ikiKatliEv.py
@@ -0,0 +1,92 @@
+from etkinlik import *
+
+#kare çizimi
+ciz(50) #50 birim ilk kenarı çiz
+solaDon() #kuzey yönüne dön
+ciz(50) #50 birim ikinci kenarı çiz
+solaDon()  #batı yönüne dön
+ciz(50) #50 birim üçüncü kenarı çiz
+solaDon() #guney yönüne dön
+ciz(50) #50 birim dördüncü kenarı çiz
+solaDon()#doğu yönüne dön
+#bir sonraki katın çizilmeye başlanacağı
+#noktaya git
+solaDon()#kuzey yönüne dön
+git(50)  #Karenin sol üst köşesine git
+sagaDon()#Üçgen çizimine başlamak için
+         #doğu yönüne dön
+#kare çizimi
+ciz(50) #50 birim ilk kenarı çiz
+solaDon() #kuzey yönüne dön
+ciz(50) #50 birim ikinci kenarı çiz
+solaDon()  #batı yönüne dön
+ciz(50) #50 birim üçüncü kenarı çiz
+solaDon() #guney yönüne dön
+ciz(50) #50 birim dördüncü kenarı çiz
+solaDon()#doğu yönüne dön
+#üçgenin çizilmeye başlanacağı
+#noktaya git
+solaDon()#kuzey yönüne dön
+git(50)  #Karenin sol üst köşesine git
+sagaDon()   #Üçgen çizimine başlamak için
+         #doğu yönüne dön
+#üçgen çizimi
+ciz(50)      #50 birim üçgen
+             #tabanını çiz
+ucgenSag(50) #50 birim üçgen
+             #sağ kenarını çiz
+ucgenSol(50) #50 birim üçgen
+             #sol kenarını çiz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#dogu()
+#git(50)
+#kuzey()
+#git(50)
+
+
+#ciz(50)
+#bati()
+#ciz(50)
+#guney()
+#ciz(50)
+
+
+#catiSag(50)
+#git(50)
+#turtle.seth(240)
+#turtle.fd(50)
+#catiSol(50)
+
