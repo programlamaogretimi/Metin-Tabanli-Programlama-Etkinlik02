23ed9c33444455870c3a5ff287b5c0f844ecedff
Metin Tabanlı Programlama Öğretimi <45932891+programlamaogretimi@users.noreply.github.com>
2019-03-16 20:02:33 +0300

    Add files via upload

---
+++ bilgiislemseldusunme-ucgenCizimi.py
@@ -0,0 +1,66 @@
+from etkinlik import *
+
+#üçgen çizimi
+ciz(50)      #50 birim üçgen
+             #tabanını çiz
+ucgenSag(50) #50 birim üçgen
+             #sağ kenarını çiz
+ucgenSol(50) #50 birim üçgen
+             #sol kenarını çiz
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#dogu()
+#git(50)
+#kuzey()
+#git(50)
+
+
+#ciz(50)
+#bati()
+#ciz(50)
+#guney()
+#ciz(50)
+
+
+#catiSag(50)
+#git(50)
+#turtle.seth(240)
+#turtle.fd(50)
+#catiSol(50)
+
