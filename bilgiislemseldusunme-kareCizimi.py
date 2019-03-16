c0d924b19a28db27bbbffa53700ab523f8c035de
Metin Tabanlı Programlama Öğretimi <45932891+programlamaogretimi@users.noreply.github.com>
2019-03-16 20:03:04 +0300

    Add files via upload

---
+++ bilgiislemseldusunme-kareCizimi.py
@@ -0,0 +1,63 @@
+from etkinlik import *
+
+#kare çizimi
+ciz(50) #50 birim ilk kenarı çiz
+solaDon() #kuzey yönüne dön
+ciz(50) #50 birim ikinci kenarı çiz
+solaDon()  #batı yönüne dön
+ciz(50) #50 birim üçüncü kenarı çiz
+solaDon() #guney yönüne dön
+ciz(50) #50 birim dördüncü kenarı çiz
+solaDon() #başlangıç guney yönüne dön
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#dogu()
+#git(50)
+#kuzey()
+#git(50)
+
+
+#ciz(50)
+#bati()
+#ciz(50)
+#guney()
+#ciz(50)
+
+
+#catiSag(50)
+#git(50)
+#turtle.seth(240)
+#turtle.fd(50)
+#catiSol(50)
+
